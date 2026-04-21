48885d62d73c97728b1965e3839171677ac1ba17
Victor <victorm.martinezjr@outlook.com>
2026-04-21 01:26:32 -0400

    create methods to render initial hero catalog upon page load

--- scripts.js
+++ scripts.js
@@ -1,10 +1,61 @@
+import { heroes, strikeSquad } from "./data.js";
+
 /**
  * --------------------------------------------------------------------------
  *                            Query Selectors
  * --------------------------------------------------------------------------
  */
+// Navbar elements
 const menuBtn = document.querySelector(".main-nav__burger");
 const menuList = document.querySelector(".main-nav__links");
+
+// Catalog elements
+const catalogGrid = document.querySelector(".catalog__grid");
+
+/**
+ * --------------------------------------------------------------------------
+ *                            Methods
+ * --------------------------------------------------------------------------
+ */
+
+// Check if hero exists in strike squad
+const existsInStrikeSquad = (heroId) => {
+  return strikeSquad.some((hero) => hero.id === heroId);
+};
+
+// Hero card component
+const createHeroCardHTML = (hero) => {
+  const isInStrikeSquad = existsInStrikeSquad(hero.id);
+
+  let buttonText = isInStrikeSquad ? "- Strike Squad " : "+ Strike Squad";
+
+  return `
+        <div class="catalog__herocard">
+          <img
+            class="catalog__herocard__img"
+            src=${hero.image}
+            alt=${hero.name}
+            />
+
+            <div class="catalog__herocard__info">
+              <h3 class="catalog__herocard__info__name">${hero.name}</h3>
+              <button class="catalog__herocard__info__button">${buttonText}</button>
+            </div>
+        </div>
+    `;
+};
+
+// Render/Update catalog with updated data
+const renderCatalog = (heroes) => {
+  catalogGrid.innerHTML = "";
+
+  heroes.forEach((hero) => {
+    catalogGrid.innerHTML += createHeroCardHTML(hero);
+  });
+};
+
+// Inital render
+renderCatalog(heroes);
 
 /**
  * --------------------------------------------------------------------------
