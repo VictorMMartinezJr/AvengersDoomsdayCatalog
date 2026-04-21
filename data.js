c5da68d8f8306f4142166fe1898bbd5efc8849ae
Victor <victorm.martinezjr@outlook.com>
2026-04-21 00:40:26 -0400

    create data.js with hero data and strike squad data

---
+++ data.js
@@ -0,0 +1,124 @@
+export const heroes = [
+  /* ==================== 
+            X-MEN 
+     ==================== */
+  {
+    id: 1,
+    name: "Cyclops",
+    role: "Tactical Field Leader",
+    team: "X-Men",
+    image: "assets/teams/xmen/cyclops.png",
+  },
+  {
+    id: 2,
+    name: "Gambit",
+    role: "Kinetic Energy Specialist",
+    team: "X-Men",
+    image: "assets/teams/xmen/gambit.png",
+  },
+  {
+    id: 3,
+    name: "Wolverine",
+    role: "Frontline Bruiser",
+    team: "X-Men",
+    image: "assets/teams/xmen/wolverine.png",
+  },
+  {
+    id: 4,
+    name: "Magneto",
+    role: "Master of Magnetism",
+    team: "X-Men",
+    image: "assets/teams/xmen/magneto.png",
+  },
+
+  /* ==================== 
+            F4
+     ==================== */
+  {
+    id: 5,
+    name: "Reed Richards",
+    role: "The Genius",
+    team: "Fantastic Four",
+    image: "assets/teams/f4/reed.png",
+  },
+  {
+    id: 6,
+    name: "Sue Storm",
+    role: "Stealth & Defense",
+    team: "F4",
+    image: "assets/teams/f4/sue.png",
+  },
+  {
+    id: 7,
+    name: "Johnny Storm",
+    role: "Aerial Assault",
+    team: "F4",
+    image: "assets/teams/f4/johnny.png",
+  },
+  {
+    id: 8,
+    name: "Ben Grimm",
+    role: "Heavy Hitter",
+    team: "F4",
+    image: "assets/teams/f4/thing.png",
+  },
+
+  /* ==================== 
+        OG AVENGERS 
+     ==================== */
+  {
+    id: 9,
+    name: "Thor",
+    role: "God of Thunder",
+    team: "OG Avengers",
+    image: "assets/teams/og-avengers/thor.png",
+  },
+  {
+    id: 10,
+    name: "Hulk",
+    role: "Gamma Scientist / Tank",
+    team: "OG Avengers",
+    image: "assets/teams/og-avengers/hulk.png",
+  },
+  {
+    id: 11,
+    name: "Captain America (Steve Rogers)",
+    role: "Leader",
+    team: "OG Avengers",
+    image: "assets/teams/og-avengers/cap.png",
+  },
+  {
+    id: 12,
+    name: "Doctor Strange",
+    role: "Master of Mystic Arts",
+    team: "OG Avengers",
+    image: "assets/teams/og-avengers/strange.png",
+  },
+
+  /* ==================== 
+        NEW AVENGERS 
+     ==================== */
+  {
+    id: 13,
+    name: "Bucky Barnes",
+    role: "Tactical Infiltrator",
+    team: "New Avengers",
+    image: "assets/teams/new-avengers/bucky.png",
+  },
+  {
+    id: 14,
+    name: "Spider-Man",
+    role: "Multiversal Specialist",
+    team: "New Avengers",
+    image: "assets/teams/new-avengers/spiderman.png",
+  },
+  {
+    id: 15,
+    name: "Sentry",
+    role: "The Golden Guardian",
+    team: "New Avengers",
+    image: "assets/teams/new-avengers/sentry.png",
+  },
+];
+
+export const strikeSquad = [];
